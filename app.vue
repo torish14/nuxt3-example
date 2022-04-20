8cf4065c7f731d830c7c9f3bea622f1c0b888310
torish14 <nanamin.104@icloud.com>
2022-04-20 23:08:52 +0900

    🎉 initial: created nuxt3 example with yarn v3

---
+++ app.vue
@@ -0,0 +1,18 @@
+<script setup>
+const version = 3
+</script>
+
+<template>
+	<div class="hello-world">
+	Hello Nuxt {{ version }}!
+	</div>
+</template>
+
+<style scoped>
+.hello-world {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 3rem;
+	color: #00dc82;
+	padding: 10rem;
+}
+</style>
